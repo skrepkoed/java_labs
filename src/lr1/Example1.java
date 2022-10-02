09a2451c4c9ee519c7bf254b3227cd626915d8cd
skrepkoed <faust898@mail.ru>
2022-10-02 21:15:32 +0500

    Examples added

---
+++ src/lr1/Example1.java
@@ -0,0 +1,9 @@
+package lr1;
+
+public class Example_1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ src/lr1/Example2.java
@@ -0,0 +1,14 @@
+package lr1;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		int num; 
+		num = 100; 
+		System.out.println("num: " + num);
+		num = num * 2;
+		System.out.print("Znachenie num * 2 равно ");
+		System.out.println(num);
+	}
+
+}
