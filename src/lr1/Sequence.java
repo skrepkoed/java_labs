858904f574699922e6b13a51095e1eaf5763787e
skrepkoed <faust898@mail.ru>
2022-10-02 20:03:03 +0500

    Task 10 completed

--- src/lr1/Sequence.java
+++ src/lr1/Sequence.java
@@ -8,10 +8,13 @@ public class Sequence {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter number a: ");
 		double a =input.nextDouble();
-		System.out.println("Number less than a by 1: "+(a-1));
+		double b = a-1;
+		double c = a+1;
+		double d = Math.pow(a+b+c,2);
+		System.out.println("Number less than a by 1: "+ b);
 		System.out.println("Number a: "+a);
-		System.out.println("Number more than a by 1: "+(a+1));
-		System.out.println("Square of the first three numbers: "+(Math.pow((a-1),2)+Math.pow((a+1),2)+Math.pow(a,2)));
+		System.out.println("Number more than a by 1: "+c);
+		System.out.println("Square of the sum of the first three numbers: "+d);
 		input.close();
 	}
 
