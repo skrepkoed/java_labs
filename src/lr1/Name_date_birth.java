f892796fc666ed43e8a20b31e1620091c2df0bd6
skrepkoed <faust898@mail.ru>
2022-10-02 19:13:18 +0500

    Task 6 completed

---
+++ src/lr1/Name_date_birth.java
@@ -0,0 +1,22 @@
+package lr1;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class Name_date_birth {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter your name:");
+		String name = input.nextLine();
+		name=name.substring(0,1).toUpperCase()+name.substring(1)+". ";
+		System.out.println("Enter your year of birth");
+		int year_of_birth = input.nextInt();
+		int current_year=LocalDate.now().getYear();
+		int age = current_year-year_of_birth;
+		System.out.println("Hello "+name+"Your age is:"+age);
+		input.close();
+
+	}
+
+}
