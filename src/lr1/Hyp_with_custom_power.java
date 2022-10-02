d53c6ecd54bb8fd1da1d45209e4e62d812074ec7
skrepkoed <faust898@mail.ru>
2022-10-02 20:22:54 +0500

    Task 11  completed

---
+++ src/lr1/Hyp_with_custom_power.java
@@ -0,0 +1,15 @@
+package lr1;
+
+public class Hyp_with_custom_power {
+	static double a =3.0, b=4.0, c;
+	public static void main(String[] args) {
+		System.out.println("katet а=" + a);
+		System.out.println("katet b=" + b);
+		System.out.println("hypotenuse с=" + hyp());
+
+	}
+	public static double hyp(){
+		return c = Math.sqrt(Math.exp(2*Math.log(a)) + Math.exp(2*Math.log(b)));
+		}
+
+}
