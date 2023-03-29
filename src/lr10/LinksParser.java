d01367c08a75d32585d38b38c8bf39e2b3b068ff
skrepkoed <faust898@mail.ru>
2023-03-29 11:32:12 +0500

    Additions to LinksParser and ExcelReader

--- src/lr10/LinksParser.java
+++ src/lr10/LinksParser.java
@@ -1,21 +1,54 @@
 package lr10;
-
-import org.jsoup.nodes.*;
 import org.jsoup.select.Elements;
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class LinksParser {
     public static void main(String[] args) {
         String url="https://itlearn.ru/first-steps";
+        out:
+        for(int i=0;i<3;i++){
         try {
-            Document doc = Jsoup.connect(url).get();
+            get_links(url);
+            break out;
+        } catch (IOException e) {
+            System.out.println("Ошибка при получении HTML-кода страницы: "+e);
+            
+                System.out.println("Попытка подключения №"+(i+1));
+                try {
+                    Thread.sleep(5000);    
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+                System.out.println("Подключение не удалось"); 
+        }
+        }
+    }
+
+    public static void get_links (String url) throws IOException {
+        Document doc = Jsoup.connect(url).get();
             Elements links = doc.select("a[href]");
+            try {
+                FileWriter writer = new FileWriter("src/lr10/file.txt");
+                BufferedWriter buffer = new BufferedWriter(writer);
+    
+                for (Element elementLinkElement : links) {
+                    buffer.write(elementLinkElement.attr("abs:href"));
+                    buffer.newLine();
+                }
+    
+                buffer.close();
+            } catch (IOException e) {
+                
+                e.printStackTrace();
+            }
             for (Element elementLinkElement : links) {
                 System.out.println(elementLinkElement.attr("abs:href"));
                 
             }
-        } catch (Exception e) {
-            // TODO: handle exception
-        }
     }
 }
--- src/lr10/ReadExcelFileExample.java
+++ src/lr10/ReadExcelFileExample.java
@@ -1,30 +1,51 @@
 package lr10;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
-
-import org.apache.logging.log4j.core.tools.picocli.CommandLine.Help.TextTable.Cell;
-import org.apache.poi.common.usermodel.*;
+import java.util.Scanner;
 import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.*;
 public class ReadExcelFileExample {
     public static void main(String[] args) {
-        String filepath="src/lr10/example3.xlsx";
+        Scanner inputScanner =new Scanner(System.in);
+        workbookAtempt:
+        for(int i=0, k=0;i<3;i++,k++){
         try {
-            FileInputStream inputStream = new FileInputStream(filepath);
+            System.out.println("Укажите путь до файла: ");
+            FileInputStream inputStream = new FileInputStream(inputScanner.nextLine());
             XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
-            XSSFSheet sheet = workbook.getSheet("Товары");
-            for (Row row : sheet) {
-                for (org.apache.poi.ss.usermodel.Cell cell : row) {
-                    System.out.println(cell.toString()+"\t");
-                }
-                System.out.println();
-            }
+            System.out.println("Введите название листа: ");
+                XSSFSheet sheet = workbook.getSheet(inputScanner.nextLine());
+                for (Row row : sheet) {
+                    for (org.apache.poi.ss.usermodel.Cell cell : row) {
+                        System.out.println(cell.toString()+"\t");
+                    }
+                    System.out.println();
+                }  
             workbook.close();
-            inputStream.close();    
+            inputStream.close();
+            break workbookAtempt;    
         } catch (Exception e) {
-            e.printStackTrace();
+            String ex_name=e.getClass().getSimpleName();
+            switch (ex_name) {
+                case "NullPointerException":
+                    if (i<2) {
+                        System.out.println("Такого листа не существует в данной книге. Попробуйте еще раз");
+                    }else{
+                        System.out.println("Попытки кончились");
+                    }    
+                break;
+
+                case "FileNotFoundException":
+                if (k<2) {
+                    System.out.println("Файл не найден. Попробуйте еще раз");
+                }else{
+                    System.out.println("Попытки кончились");
+                }
+                break;
+            
+                default:
+                    break;
+            }
         }
-        
+    }     
     }
 }
